8f9572d1fea1f3c97e616fd0d15dc2c92adc9267
Sashaperepel <124892766+Sashaperepel@users.noreply.github.com>
2023-02-25 16:40:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,15 +8,13 @@ bool checkPrime(uint64_t value) {
     int count;
     count = 0;
     for (int i = 1; i * i <= value; ++i) {
-        if ((value % i == 0) && (value != 1)) {
+        if ((value % i == 0) && (value != 1))
             ++count;
-        }
     }
     if (count == 1)
         return true;
-    else {
+    else
         return false;
-    }
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -25,26 +23,22 @@ uint64_t nPrime(uint64_t n) {
     value = 1;
     count = 0;
     while (n > count) {
-        if (checkPrime(value)) {
+        if (checkPrime(value))
             ++count;
-        }
         ++value;
     }
-    if (n == 0) {
+    if (n == 0)
         return 0;
-    }
     return --value;
 }
 
 uint64_t nextPrime(uint64_t value) {
     // вставьте код функции
     ++value;
-    if (checkPrime(value)) {
+    if (checkPrime(value))
         return value;
-    }   
-    else {
+    else
         nextPrime(value);
-    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
