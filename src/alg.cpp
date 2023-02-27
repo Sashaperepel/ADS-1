b849b0ee73ad47eee466de85e0966e773daf69ed
Sashaperepel <124892766+Sashaperepel@users.noreply.github.com>
2023-02-27 09:47:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,7 +19,7 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
     // вставьте код функции
-    int value, count;
+    uint64_t value, count;
     value = 1;
     count = 0;
     while (n > count) {
@@ -43,7 +43,7 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
     // вставьте код функции
-    int sum = 0;
+    uint64_t sum = 0;
     for (int i = 1; i < hbound; i++) {
         if (checkPrime(i))
             sum = sum + i;
